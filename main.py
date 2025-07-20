a6d272719fb7b8233b68a3a57de48cc4ddf1dcdd
Mahad Zulfiqar <mahadzulfiqar2@gmail.com>
2025-07-20 15:39:18 +0500

    main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-from active.banner_grab import grab_banner
-from active.port_scan import scan_ports
-from active.tech_detect import detect_tech
-from passive.dns_enum import get_dns_records
-from passive.subdomain_enum import get_subdomains
-from passive.whois_lookup import whois
+import os
 
-def run_all(target):
-    print("\n--- Active Recon ---")
-    scan_ports(target, [21, 22, 80, 443, 8080])
-    grab_banner(target, 80)
-    detect_tech(f"http://{target}")
+print("=== Custom Recon Tool ===")
+target = input("Enter the target (domain or IP): ")
 
-    print("\n--- Passive Recon ---")
-    get_dns_records(target)
-    get_subdomains(target)
-    print(whois(target))
-Added main.py controller
+print("\n--- ACTIVE RECON ---")
+os.system(f"python active/port_scan.py")
+os.system(f"python active/banner_grab.py")
+os.system(f"python active/tech_detect.py")
+
+print("\n--- PASSIVE RECON ---")
+os.system(f"python passive/whois_lookup.py")
+os.system(f"python passive/dns_enum.py")
+os.system(f"python passive/subdomain_enum.py")
