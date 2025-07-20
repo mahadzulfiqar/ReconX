c33f9a2d3e2d3ebe153f9eb915778a31386ade9e
Mahad Zulfiqar <mahadzulfiqar2@gmail.com>
2025-07-20 15:30:40 +0500

    port_scan.py

--- active/active/port_scan.py
+++ active/active/port_scan.py
@@ -1,1 +1,18 @@
+import socket
 
+def scan_ports(host, ports):
+    print(f"Scanning ports on {host}")
+    for port in ports:
+        try:
+            s = socket.socket()
+            s.settimeout(1)
+            s.connect((host, port))
+            print(f"[+] Port {port} is OPEN")
+            s.close()
+        except:
+            pass
+
+if _name_ == "_main_":
+    target = input("Enter target host (e.g., example.com): ")
+    ports = [21, 22, 80, 443, 8080]
+    scan_ports(target, ports)
